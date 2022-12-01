bd121f1f245d83ff3ca77fd39d387e07745d8cae
urke90 <urosbijelic90@gmail.com>
2022-12-01 15:04:13 +0100

    created axios instance and testd on /books url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
+
+import axios from './api/axios.config';
+
 import './App.scss';
 
 const App = () => {
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const response = await axios.request({
+                    url: 'https://book-store.mvsoft.co.rs/books',
+                    method: 'get'
+                });
+                console.log('response', response);
+            } catch (error) {}
+        };
+        fetchBooks();
+    }, []);
+
     return <div className="app">testing initi</div>;
 };
 
