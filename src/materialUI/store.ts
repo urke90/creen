c2bd3ceedf93c83054fe657bfd2cd228b1c2155d
urke90 <urosbijelic90@gmail.com>
2022-12-02 16:10:36 +0100

    added material UI

---
+++ src/materialUI/store.ts
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/system';
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#0b52a2',
+            dark: '#002b73'
+        }
+    }
+});
