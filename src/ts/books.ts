c9a3a6981cb8522be3c65037694d37a8c1390823
urke90 <urosbijelic90@gmail.com>
2022-12-05 21:28:37 +0100

    fetched books saved to state, created interface for book

---
+++ src/ts/books.ts
@@ -0,0 +1,11 @@
+export interface IBook {
+    id: number;
+    isbn: number;
+    title: string;
+    nameOfAuthor: string;
+    dateOfBirthAuthor: string;
+    quantity: number;
+    numOfPages: number;
+    coverPhoto: string;
+    yearOfBublishing: number;
+}
